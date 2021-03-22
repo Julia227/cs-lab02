dd7e97a4ec084459647620cbfa66c3a9a13223df
Alice (Grebeneva JI) <grebenevaju@yandex.ru>
2021-03-22 10:37:08 +0300

    code: вывод суммы и разности a и b

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
 cout << "Enter A and B: ";
-    int a, b, sum;
+    int a, b;
     cin >> a >> b;
-    sum = a + b;
-    cout << sum;
+    cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n';
     return 0;
 }
